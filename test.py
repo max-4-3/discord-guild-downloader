b01a296e1d605f764378013cb3fba3f94c8b357d
Maximun <132590174+max-4-3@users.noreply.github.com>
2024-09-26 20:43:01 +0530

    Made Changed

--- test.py
+++
@@ -1,4 +0,0 @@
-text = "file, file2, opt, rew"
-
-print("\n".join([f"\t{k.strip()}".expandtabs(4) for k in text.split(",") if k]))
-
--- utility/__init__.py
+++ utility/__init__.py
@@ -1,4 +1,9 @@
 import os
 from platform import system as os_name
 
-CONFIG_PATH = os.path.join(os.getenv('LOCALAPPDATA'), 'config_file.json') if os_name().lower() in ['windows', 'nt'] else os.path.join(os.getcwd(), 'config_file.json')
+_path = os.path.split(os.path.split(__file__)[0])[0]
+is_windows = True if os_name().lower() in ['windows', 'nt'] else False
+CONFIG_PATH = os.path.join(os.getenv('LOCALAPPDATA'), "discord-guild-downloader",'config_file.json') if is_windows else os.path.join(os.getenv('HOME', _path), "discord-guild-downloader", 'config_file.json')
+
+if not os.path.exists(CONFIG_PATH):
+	os.makedirs(CONFIG_PATH, exist_ok=True)
