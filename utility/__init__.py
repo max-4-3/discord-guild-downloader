593501e471bf568c56aa9c410515fe1e43866635
Maximun <132590174+max-4-3@users.noreply.github.com>
2024-09-26 20:51:55 +0530

    Made Changes

--- utility/__init__.py
+++ utility/__init__.py
@@ -5,5 +5,5 @@ _path = os.path.split(os.path.split(__file__)[0])[0]
 is_windows = True if os_name().lower() in ['windows', 'nt'] else False
 CONFIG_PATH = os.path.join(os.getenv('LOCALAPPDATA'), "discord-guild-downloader",'config_file.json') if is_windows else os.path.join(os.getenv('HOME', _path), "discord-guild-downloader", 'config_file.json')
 
-if not os.path.exists(CONFIG_PATH):
-	os.makedirs(CONFIG_PATH, exist_ok=True)
+if not os.path.exists(os.path.dirname(CONFIG_PATH)):
+	os.makedirs(os.path.dirname(CONFIG_PATH), exist_ok=True)
